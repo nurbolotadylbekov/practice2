4e084171d149faca6a1106505c75e9ffe0105a94
nurbolotadylbekov <adylbekov.n@gmail.com>
2023-03-29 20:16:35 +0600

    practice 2

---
+++ practice2.dart
@@ -0,0 +1,90 @@
+void main() {
+  num num1;
+  num1 = 3 + 5;
+  print(num1);
+
+  num num2;
+  num2 = 3;
+  print(num2);
+
+  num num3;
+  num3 = 3.5;
+  print(num3);
+
+  num num4;
+  num4 = 3 / 2;
+  print(num4);
+
+  num num5;
+  num5 = 3 * 2;
+  print(num5);
+
+  int int1 = 47;
+  int int2 = 369;
+  int int3 = 3;
+  int int4 = 1000;
+  int int5 = 999;
+  int1 = int1 + 2;
+  int5 = int4 + int2;
+  print(int1);
+  print(int2);
+  print(int3);
+  print(int4);
+  print(int5);
+
+  double dbl1 = 3.14;
+  double dbl2 = 3;
+  double dbl3 = 3.14231341;
+  double dbl4 = 456;
+  double dbl5 = 3143124214;
+  dbl5 = dbl1 * dbl2;
+  print(dbl1);
+  print(dbl2);
+  print(dbl3);
+  print(dbl4);
+  print(dbl5);
+
+  String name = "Nurbolot";
+  String city = "Bishkek";
+  String street = "Panfilova";
+  String mood = "happy";
+  String plan = "none";
+
+  print(name);
+  print(city);
+  print(street);
+  print(mood);
+  print(plan);
+
+  List line = [1, 3, "Nurbolot", 3.14];
+
+  Map info = {'name': 'Nurbolot', 'age': 28};
+
+  var symbol = '\a900';
+  print(symbol);
+
+  final z;
+  z = 5;
+  print(z);
+  final k = 4;
+  print(k);
+
+  const p = 90;
+  const o = 3.4545;
+  const i = 67;
+
+  print(p);
+  print(o);
+  print(i);
+
+  dynamic f = 3;
+  dynamic u = 3.45;
+  dynamic l = "text";
+  dynamic m = true;
+  dynamic v = false;
+  print(f);
+  print(u);
+  print(l);
+  print(m);
+  print(v);
+}
